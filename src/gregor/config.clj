f1daeded09a2db83242b88f0c0cbf3b5a4c072d8
Marcus Oladell <marcus.oladell@novolabs.com>
2019-01-31 18:53:47 -0600

    Removed things from config that have default values

--- src/gregor/config.clj
+++ src/gregor/config.clj
@@ -3,7 +3,4 @@
 (defn default-producer
   "Creates a default configuration map"
   [bootstrap-servers]
-  {:gregor.producer/input-buffer 10
-   :gregor.producer/output-buffer 100
-   :gregor.producer/timeout 200
-   :gregor.producer/kafka-config {:bootstrap.servers bootstrap-servers}})
+  {:gregor.producer/kafka-config {:bootstrap.servers bootstrap-servers}})

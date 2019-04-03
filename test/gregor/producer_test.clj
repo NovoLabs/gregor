2b05b7193ddb87a264dc64996791f6fe23e4425c
Marcus Oladell <marcus.oladell@novolabs.com>
2019-04-03 15:08:10 -0500

    Meaningless test

--- test/gregor/producer_test.clj
+++
@@ -1,7 +0,0 @@
-(ns gregor.producer-test
-  (:require [clojure.test :refer :all]
-            [gregor.producer :refer :all]))
-
-(deftest circle-ci-bootstrap
-  (testing "bootstrapping of circle-ci"
-    (is (= 1 1))))
